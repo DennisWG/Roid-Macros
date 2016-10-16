8e633a5659f2fe1033f543bb8b70246d0b7f7e42
DWG <DennisWG@users.noreply.github.com>
2016-10-16 21:45:55 +0200

    Added missing Utility.lua file

---
+++ Utility.lua
@@ -0,0 +1,34 @@
+--[[
+	Author: Dennis Werner Garske (DWG)
+	License: MIT License
+
+	Last Modified:
+        10.16.2016 (DWG): Added MMC_splitString
+]]
+--/run for k, v in ipairs(MMC_splitString("help, harm,target=player", ",%s*")) do message(v); end
+
+-- Splits the given string into a list of sub-strings
+-- str: The string to split
+-- seperatorPattern: The seperator between sub-string. May contain patterns
+-- returns: A list of sub-strings
+function MMC_splitString(str, seperatorPattern)
+    local tbl = {};
+    local pattern = "(.-)" .. seperatorPattern;
+    local lastEnd = 1;
+    local s, e, cap = string.find(str, pattern, 1);
+   
+    while s do
+        if s ~= 1 or cap ~= "" then
+            table.insert(tbl,cap);
+        end
+        lastEnd = e + 1;
+        s, e, cap = string.find(str, pattern, lastEnd);
+    end
+    
+    if lastEnd <= string.len(str) then
+        cap = string.sub(str, lastEnd);
+        table.insert(tbl, cap);
+    end
+    
+    return tbl
+end
