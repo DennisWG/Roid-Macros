4d045833247f52b693b78d61f861fd113a2e93bf
DennisWG <DennisWG@users.noreply.github.com>
2016-12-10 14:28:05 +0100

    added support for pfUI's player, target and targettarget frames

---
+++ Extensions/Mouseover/pfUI.lua
@@ -0,0 +1,80 @@
+--[[
+	Author: Dennis Werner Garske (DWG)
+	License: MIT License
+]]
+local _G = _G or getfenv(0)
+local MMC = _G.CastModifier or {}
+
+MMC.mouseoverUnit = MMC.mouseoverUnit or nil;
+
+local Extension = MMC.RegisterExtension("Grid");
+Extension.RegisterEvent("PLAYER_ENTERING_WORLD", "PLAYER_ENTERING_WORLD");
+
+function Extension.RegisterPlayerScripts()
+    local onEnterFunc = pfUI.uf.player:GetScript("OnEnter");
+    local onLeaveFunc = pfUI.uf.player:GetScript("OnLeave");
+    
+    pfUI.uf.player:SetScript("OnEnter", function()
+        MMC.mouseoverUnit = "player";
+        if onEnterFunc then
+            onEnterFunc();
+        end
+    end);
+    
+    pfUI.uf.player:SetScript("OnLeave", function()
+        MMC.mouseoverUnit = nil;
+        if onLeaveFunc then
+            onLeaveFunc();
+        end
+    end);
+end
+
+function Extension.RegisterTargetScripts()
+    local onEnterFunc = pfUI.uf.target:GetScript("OnEnter");
+    local onLeaveFunc = pfUI.uf.target:GetScript("OnLeave");
+    
+    pfUI.uf.target:SetScript("OnEnter", function()
+        MMC.mouseoverUnit = "target";
+        if onEnterFunc then
+            onEnterFunc();
+        end
+    end);
+    
+    pfUI.uf.target:SetScript("OnLeave", function()
+        MMC.mouseoverUnit = nil;
+        if onLeaveFunc then
+            onLeaveFunc();
+        end
+    end);
+end
+
+function Extension.RegisterTargetTargetScripts()
+    local onEnterFunc = pfUI.uf.targettarget:GetScript("OnEnter");
+    local onLeaveFunc = pfUI.uf.targettarget:GetScript("OnLeave");
+    
+    pfUI.uf.targettarget:SetScript("OnEnter", function()
+        MMC.mouseoverUnit = "targettarget";
+        if onEnterFunc then
+            onEnterFunc();
+        end
+    end);
+    
+    pfUI.uf.targettarget:SetScript("OnLeave", function()
+        MMC.mouseoverUnit = nil;
+        if onLeaveFunc then
+            onLeaveFunc();
+        end
+    end);
+end
+
+function Extension.PLAYER_ENTERING_WORLD()
+    Extension.RegisterPlayerScripts();
+    Extension.RegisterTargetScripts();
+    Extension.RegisterTargetTargetScripts();
+end
+
+function Extension.OnLoad()
+    if arg1 ~= "pfUI" then
+        return;
+    end
+end
