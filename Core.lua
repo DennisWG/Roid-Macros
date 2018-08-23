DennisWG/Roid-Macros

commit b3a3e0bc85e35c7ad20b70b7e5317ee5e9833636

Disable auto targeting when the Interface Option for "Auto Self Cast" is toggled off.

Core.lua
@@ -118,6 +118,10 @@ end
 -- conditionals: The conditionals containing the current target
 -- returns: Whether or not we've changed the player's current target
 function Roids.FixEmptyTarget(conditionals)
+    if GetCVar("autoSelfCast") == "0" then
+        return false;
+    end
+
     if not conditionals.target then
         if UnitExists("target") then
             conditionals.target = "target";
@@ -232,7 +236,7 @@ function Roids.DoWithConditionals(msg, hook, fixEmptyTargetFunc, targetBeforeAct
         end
     end
     
-    if targetBeforeAction then
+    if conditionals.target ~= nil and targetBeforeAction then
         if not UnitIsUnit("target", conditionals.target) then
             needRetarget = true;
         end

commit 8b132a8e56f0464f1e40e5a8844be81db8aeb526

Fix [harm], broken by b3a3e0b.

Core.lua
@@ -118,14 +118,10 @@ end
 -- conditionals: The conditionals containing the current target
 -- returns: Whether or not we've changed the player's current target
 function Roids.FixEmptyTarget(conditionals)
-    if GetCVar("autoSelfCast") == "0" then
-        return false;
-    end
-
     if not conditionals.target then
         if UnitExists("target") then
             conditionals.target = "target";
-        else
+        elseif GetCVar("autoSelfCast") == "1" then
             conditionals.target = "player";
         end
     end