5b7ee6687f0ecef2e1554ddc585b2e596b209cbe
Andrew Holbrook <atholbro@gmail.com>
2018-10-30 21:49:50 -0400

    Fixed mouseover support for NotGrid.

--- Extensions/Mouseover/NotGrid.lua
+++ Extensions/Mouseover/NotGrid.lua
@@ -4,6 +4,8 @@
 ]]
 local _G = _G or getfenv(0)
 local Roids = _G.Roids or {}
+
+local CreateFrames = nil;
 Roids.mouseoverUnit = Roids.mouseoverUnit or nil;
 
 local Extension = Roids.RegisterExtension("NotGrid");
@@ -17,30 +19,35 @@ function Extension.OnLeave()
     Roids.mouseoverUnit = nil;
 end
 
-function Extension.ConfigUnitFrame(_, frame)
-    local pattern = "%d+";
-    local name = frame:GetName();
+function Roids:NotGrid_CreateFrames()
+    CreateFrames(NotGrid); -- call the original
     
-    enter = frame:GetScript("OnEnter");
-    leave = frame:GetScript("OnLeave");
-    
-    frame:SetScript("OnEnter", function()
-        enter();
-        Extension.OnEnter();
-    end);
-    
-    frame:SetScript("OnLeave", function()
-        leave();
-        Extension.OnLeave();
-    end);
+    -- NotGrid stores all of it's frames in the NotGrid.UnitFrames table.
+    for k, frame in pairs(NotGrid.UnitFrames) do
+        local enter = frame:GetScript("OnEnter");
+        local leave = frame:GetScript("OnLeave");
+
+        frame:SetScript("OnEnter", function()
+            enter();
+            Extension.OnEnter();
+        end);
+
+        frame:SetScript("OnLeave", function()
+            leave();
+            Extension.OnLeave();
+        end);
+    end
 end
 
 function Extension.OnLoad()
-    if arg1 ~= "NotGrid" then
-        return;
+    -- NotGrid loads before Roids, so if NotGrid is enabled, then it's global will exist.
+    if not NotGrid then
+	-- Hooking manually as we need a post hook.
+        CreateFrames = NotGrid.CreateFrames;
+        NotGrid.CreateFrames = Roids.NotGrid_CreateFrames;
     end
     
-    Extension.HookMethod(_G["NotGrid"], "ConfigUnitFrame", "ConfigUnitFrame");
+    Extension.UnregisterEvent("ADDON_LOADED", "Onload")
 end
 
 _G["Roids"] = Roids;
